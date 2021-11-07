1c3d9d1bd61c2449f440eeaf2a9b57a96019d7d2
Andrii Abramov <andrey.abramov@tranzzo.com>
2021-11-07 22:26:04 +0200

    initial drop for goal cli

---
+++ go.mod
@@ -0,0 +1,9 @@
+module goal
+
+go 1.17
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/olekukonko/tablewriter"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	osexec "os/exec"
+	"strings"
+)
+
+type YamlCommand struct {
+	Cmd  string   `yaml:"cmd"`
+	Args []string `yaml:"args"`
+	Desc string   `yaml:"desc"`
+}
+
+type Command struct {
+	Name string
+	Cmd  string
+	Args []string
+	Desc string
+}
+
+func (c Command) cli() string {
+	return fmt.Sprintf("%s %s", c.Cmd, strings.Join(c.Args, " "))
+}
+
+type Commands struct {
+	commands []Command
+}
+
+func (c *Commands) get(name string) (*Command, bool) {
+	for _, command := range c.commands {
+		if command.Name == name {
+			return &command, true
+		}
+	}
+	return nil, false
+}
+
+func (c *Commands) render() {
+	fmt.Println("Available commands:")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "CLI", "Description"})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold}, tablewriter.Colors{tablewriter.Bold}, tablewriter.Colors{tablewriter.Bold})
+	table.SetColumnColor(
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgGreenColor},
+		tablewriter.Colors{tablewriter.Normal},
+		tablewriter.Colors{tablewriter.Normal},
+	)
+	for _, cmd := range c.commands {
+		table.Append([]string{cmd.Name, cmd.cli(), cmd.Desc})
+		//fmt.Printf("\t%s: '%s' #%s\n", cmd.Name, cmd.cli(), cmd.Desc)
+	}
+	table.Render()
+}
+
+func parseCommands(bytes []byte) (*Commands, error) {
+	rawCommands := map[string]YamlCommand{}
+	err := yaml.Unmarshal(bytes, &rawCommands)
+	if err != nil {
+		return nil, err
+	}
+	var res []Command
+	for name, command := range rawCommands {
+		var args []string
+		if command.Args == nil {
+			args = []string{}
+		} else {
+			args = command.Args
+		}
+		res = append(res, Command{
+			Name: name,
+			Cmd:  command.Cmd,
+			Args: args,
+			Desc: command.Desc,
+		})
+	}
+	return &Commands{commands: res}, nil
+}
+
+func main() {
+
+	defaultFilename := "goal.yaml"
+
+	if _, err := os.Stat(defaultFilename); errors.Is(err, os.ErrNotExist) {
+		panic(fmt.Sprintf("%s does not exist!", defaultFilename))
+	}
+
+	file, err := ioutil.ReadFile("goal.yaml")
+	if err != nil {
+		panic(err)
+	}
+	commands, _ := parseCommands(file)
+
+	if len(os.Args) == 1 {
+		commands.render()
+	} else {
+		name := strings.TrimSpace(os.Args[1])
+		command, exists := commands.get(name)
+		if exists {
+			exec(command)
+		} else {
+			_, _ = os.Stderr.WriteString("No such command in goal.yaml: " + name + "\n")
+		}
+	}
+
+}
+
+func exec(command *Command) {
+	cmd := osexec.Command(command.Cmd, command.Args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
+	if err != nil {
+		fmt.Println(fmt.Sprint(err))
+	}
+}
